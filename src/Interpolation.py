15a76cd493575c62721a6575f161649921b3ffb8
andi arapi <andi_arapi@hotmail.it>
2021-03-04 18:39:01 +0100

    Fix Interpolation

--- src/Interpolation.py
+++ src/Interpolation.py
@@ -13,16 +13,16 @@ def interpolateSampling(xy, z, pixel):
     plt.colorbar()
     plt.savefig(SAMPLING_PATH + "/" + str(pixel) + '.png')
 
-    rbf = scipy.interpolate.Rbf(xy[0, :], xy[1, :], z, smooth=1, function='gaussian')
-
-    XX, YY = np.meshgrid(np.linspace(-1, 1, 256), np.linspace(-1, 1, 256))
+    rbf = scipy.interpolate.Rbf(xy[0, :], xy[1, :], z, smooth=0.01, function='linear')
+    XX, YY = np.meshgrid(np.linspace(-1, 1, 64), np.linspace(-1, 1, 64))
 
     z_interpolation = rbf(XX, YY)
 
     plt.figure()
     plt.title('Learn Pixel #' + str(pixel))
-    plt.imshow(z_interpolation, vmin=0, vmax=255, cmap="gray")
+    plt.pcolormesh(XX, YY, z_interpolation, shading='flat', vmin=0, vmax=255)
     plt.colorbar()
+    plt.scatter(x=xy[0, :], y=xy[1, :], c=z, vmin=0, vmax=255)
     plt.savefig(PREDICTIONS_PATH + "/" + str(pixel) + '.png')
 
 # interpolateSampling((np.random.rand(2, 10) - 0.5) * 2, np.random.rand(1, 10) * 255)
--- src/ProjectIntenisityDirection.py
+++ src/ProjectIntenisityDirection.py
@@ -3,7 +3,8 @@ import cv2 as cv
 import numpy as np
 from src.DirectionLight import getLightDirection, drawUnitCircle
 from src.Interpolation import interpolateSampling
-from src.SamplingIntensity import getObjPoints
+import matplotlib.pyplot as plt
+
 from src.utils import loadMatrix, rescaleFrame
 
 PATH_CC_LC = "rsc/results/sampling/cc_lc"
@@ -31,14 +32,14 @@ def samplingPlotCCLC(path_cc, path_lc):
 def pixelSpecific(path_cc, path_lc):
     x1 = 90
     x2 = 145
-    y1 = 220
+    y1 = 200
     y2 = 221
 
     frame = cv.imread("rsc/results/sampling/cc/0.jpg")
     for x in range(x1, x2):
         for y in range(y1, y2):
             frame[y, x] = 0
-    cv.imwrite(PATH_PIXELS[:-7]+ "0-frame.jpg",frame)
+    cv.imwrite(PATH_PIXELS[:-7] + "0-frame.jpg", frame)
     cv.destroyAllWindows()
 
     matrix_cc = []
@@ -48,13 +49,13 @@ def pixelSpecific(path_cc, path_lc):
     matrix_lc.extend(loadMatrix(path_lc))
 
     # get points
-    pixel_cc = np.zeros((1, len(matrix_cc)), dtype=np.float32)
-    pixel_lc = np.zeros((2, len(matrix_lc)), dtype=np.float32)
     i = 0
 
     for x in tqdm(range(x1, x2), desc="Sampling"):
         for y in range(y1, y2):
             img = drawUnitCircle()
+            pixel_cc = np.zeros((1, len(matrix_cc)), dtype=np.float32)
+            pixel_lc = np.zeros((2, len(matrix_lc)), dtype=np.float32)
             for n_frame in range(len(matrix_lc)):
                 if matrix_lc[n_frame].size > 3:
                     reshaped_matrix_lc = np.reshape(matrix_lc[n_frame], (2, 640, 480))
@@ -64,7 +65,13 @@ def pixelSpecific(path_cc, path_lc):
                     pixel_lc[:, n_frame] = lc_pixel
                     new_img = getLightDirection(lc_pixel, i_pixel)
                 img = cv.add(img, new_img)
-            cv.imwrite(PATH_PIXELS + str(i) + ".jpg", img)
+            pixel_cc = pixel_cc[:, :pixel_lc.shape[-1]]
+            good_indicies = pixel_cc.flatten() != 0
+            pixel_cc = pixel_cc[:, good_indicies]
+            pixel_lc = pixel_lc[:, good_indicies]
+
+            # plt.hist(pixel_cc.flatten())
+            # plt.show()
+            cv.imwrite(PATH_PIXELS + str(i) + ".jpg", rescaleFrame(img, 4))
             interpolateSampling(pixel_lc[:, :201], pixel_cc[:, :201], i)
             i = i + 1
-
